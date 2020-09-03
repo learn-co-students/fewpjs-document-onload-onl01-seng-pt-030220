1e85c6985e28d96bc0ef362c9d7a4d2cc9547443
github username <github email address>
2020-09-03 22:04:05 +0100

    Done.

--- index.js
+++ index.js
@@ -5,13 +5,3 @@ document.addEventListener("DOMContentLoaded", function(){
     input.innerHTML = "This is really cool!";
   });
    
-
-//   console.log(
-//     "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
-//   );
-
-
-function meow(){
-  const input = document.getElementById("text")
-  input.innerHTML = "This is really cool!"
-};
